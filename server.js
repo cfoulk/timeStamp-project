8de788c3e012373f0f838cb91369a947de1aa443
cfoulk <charlesfoulk@csus.edu>
2021-11-08 05:30:08 -0800

    hopefully last?

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.get("/api/:timeStamp", (req, res) => {
   let timeStamp = req.params.timeStamp;
   //checks if it has a -
   if (!timeStamp.match(/-/g)) {
-    timeStamp = +timeStamp; //converts this into a number, so that our apiDate assignment can be processed correctly
+    timeStamp = parseInt(timeStamp);
+    //timeStamp = +timeStamp; //converts this into a number, so that our apiDate assignment can be processed correctly
   }
   let apiDate = new Date(timeStamp);
   if (apiDate.toUTCString() == "Invalid Date") {
